56ffa8951a68439b1e568c5ed6b0a642ba59c955
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2023-09-27 00:18:50 +0800

    feat: add explanation on prisma module

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,9 +1,11 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
+// Global decorator is useed to inform that the module will be used globally
 @Global()
 @Module({
   providers: [PrismaService],
+  // export PrismaService is used to make the service can be access by other controller or service
   exports: [PrismaService],
 })
 export class PrismaModule {}
