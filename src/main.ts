cb312ddfc98526aba8806430d2ad86e133e46fd1
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2023-09-27 00:19:37 +0800

    feat: add whitelist

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function runApplication() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // Whitelist is used to filter the request body property that sent to the server but we don't define
+      whitelist: true,
+    }),
+  );
   await app.listen(process.env.NEST_PORT || 3000);
 }
 
