9c8d465c4e41619e9a9c0e2b9e32a121ad4a555f
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2023-09-27 00:28:58 +0800

    feat: filter use select in prisma

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,36 +1,58 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import * as argon from 'argon2';
 
 import { PrismaService } from '../prisma/prisma.service';
 import { InsertUserDto, IDeleteUser, IUser } from './dto';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable({})
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
   async getUserById(id: string): Promise<IUser> {
-    const user = await this.prisma.user.findFirst({
+    return this.prisma.user.findFirst({
       where: {
         id,
       },
+      // Select is used to choose selected field to be return
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        password: true, // Better don't expose the password
+      },
     });
-
-    return this.generateUserResponse(user);
   }
 
   async insertUser(dto: InsertUserDto): Promise<IUser> {
     // optional, it'd be great if we hash the password in the database instead of plain text
     const hashPassword = await argon.hash(dto.password);
 
-    const user = await this.prisma.user.create({
-      data: {
-        name: dto.name,
-        email: dto.email,
-        password: hashPassword,
-      },
-    });
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          name: dto.name,
+          email: dto.email,
+          password: hashPassword, // Better don't expose the password
+        },
+        // Select is used to choose selected field to be return
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          password: true, // Better don't expose the password
+        },
+      });
 
-    return this.generateUserResponse(user);
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Email is taken');
+        }
+      }
+      throw error;
+    }
   }
 
   async removeUserById(id: string): Promise<IDeleteUser> {
@@ -43,15 +65,6 @@ export class UsersService {
     return {
       success: true,
       message: `Success delete user with ID ${id}`,
-    };
-  }
-
-  generateUserResponse(user): IUser {
-    return {
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      password: user.password,
     };
   }
 }
